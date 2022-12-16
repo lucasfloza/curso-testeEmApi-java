fbe9d47ff5c8dcf6c042c9874152548e01fff6e0
Lucas Floza <lucasfsouza.l10@gmail.com>
2022-12-16 15:15:27 -0300

    Exception handling for object not found.

--- src/main/java/br/com/testeemapi/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/testeemapi/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.testeemapi.api.services.impl;
 import br.com.testeemapi.api.domain.User;
 import br.com.testeemapi.api.repositories.UserRepository;
 import br.com.testeemapi.api.services.UserService;
+import br.com.testeemapi.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow (()-> new ObjectNotFoundException("Objeto não encontrado!"));
     }
 
 
