1fe2ee31f73c5d65b7dabf442045b0c87399cba7
Lucas Floza <lucasfsouza.l10@gmail.com>
2022-12-17 14:51:14 -0300

    Create endpoint findAll for User.

--- src/main/java/br/com/testeemapi/api/services/UserService.java
+++ src/main/java/br/com/testeemapi/api/services/UserService.java
@@ -2,6 +2,9 @@ package br.com.testeemapi.api.services;
 
 import br.com.testeemapi.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     User findById(Integer id);
+    List<User> findAll();
 }
