61a19e423ede56fa6a45c038553f45ac8e55abc4
Lucas Floza <lucasfsouza.l10@gmail.com>
2022-12-17 14:26:43 -0300

    Return data from UserDTO to UserResource

--- src/main/java/br/com/testeemapi/api/resource/UserResource.java
+++ src/main/java/br/com/testeemapi/api/resource/UserResource.java
@@ -2,7 +2,9 @@ package br.com.testeemapi.api.resource;
 
 
 import br.com.testeemapi.api.domain.User;
+import br.com.testeemapi.api.domain.dto.UserDTO;
 import br.com.testeemapi.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource{
 
         @Autowired
+        private ModelMapper mapper;
+
+        @Autowired
         private UserService service;
 
         @GetMapping(value = "/{id}")
-        public ResponseEntity<User> findById(@PathVariable Integer id){
-            return ResponseEntity.ok().body(service.findById(id));
+        public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+            return ResponseEntity.ok().body(mapper.map(service.findById(id),UserDTO.class));
         }
 }
