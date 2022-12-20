37970e49f6d6cccd486493ef1d68d85b3a98be71
Lucas Floza <lucasfsouza.l10@gmail.com>
2022-12-20 14:49:33 -0300

    Test the dataIntegrityViolationException() method of the ResourceExceptionHandler class.

--- src/main/java/br/com/testeemapi/api/services/exceptions/DataIntegratyViolationException.java
+++
@@ -1,7 +0,0 @@
-package br.com.testeemapi.api.services.exceptions;
-
-public class DataIntegratyViolationException extends RuntimeException {
-    public DataIntegratyViolationException(String message) {
-        super(message);
-    }
-}
