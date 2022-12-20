f9375504db584dd3aa255f447402cf6f6d5eec22
Lucas Floza <lucasfsouza.l10@gmail.com>
2022-12-20 08:48:02 -0300

    Testing the endpoint of the findAll() method implemented in the UserResource(Controller) class.

--- src/test/java/br/com/testeemapi/api/resource/UserResourceTest.java
+++ src/test/java/br/com/testeemapi/api/resource/UserResourceTest.java
@@ -12,8 +12,11 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -54,14 +57,14 @@ class UserResourceTest {
     void whenFindByIdThenReturnSuccess() {
 
         when(userServiceImpl.findById(anyInt())).thenReturn(user);
-        when(mapper.map(any(),any())).thenReturn(userDTO);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
 
         ResponseEntity<UserDTO> response = resource.findById(ID);
 
         assertNotNull(response);
         assertNotNull(response.getBody());
-        assertEquals(ResponseEntity.class,response.getClass());
-        assertEquals(UserDTO.class,response.getBody().getClass());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
 
         assertEquals(ID, response.getBody().getId());
         assertEquals(EMAIL, response.getBody().getEmail());
@@ -70,7 +73,25 @@ class UserResourceTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAListOfUserDTO() {
+
+        when(userServiceImpl.findAll()).thenReturn(List.of(user));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = resource.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class,response.getBody().getClass());
+        assertEquals(UserDTO.class,response.getBody().get(0).getClass());
+
+        assertEquals(ID, response.getBody().get(0).getId());
+        assertEquals(EMAIL, response.getBody().get(0).getEmail());
+        assertEquals(NAME, response.getBody().get(0).getName());
+        assertEquals(PASSWORD, response.getBody().get(0).getPassword());
+
     }
 
     @Test
