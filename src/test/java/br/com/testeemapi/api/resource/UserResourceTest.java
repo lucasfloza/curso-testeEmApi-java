5adcda880505ca467bebd903bf42a65bdc9a32f6
Lucas Floza <lucasfsouza.l10@gmail.com>
2022-12-20 08:27:20 -0300

    Testing the endpoint of the findById() method implemented in the UserResource(Controller) class.

--- src/test/java/br/com/testeemapi/api/resource/UserResourceTest.java
+++ src/test/java/br/com/testeemapi/api/resource/UserResourceTest.java
@@ -3,17 +3,24 @@ package br.com.testeemapi.api.resource;
 import br.com.testeemapi.api.domain.User;
 import br.com.testeemapi.api.domain.dto.UserDTO;
 import br.com.testeemapi.api.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserResourceTest {
@@ -44,7 +51,22 @@ class UserResourceTest {
     }
 
     @Test
-    void findById() {
+    void whenFindByIdThenReturnSuccess() {
+
+        when(userServiceImpl.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(),any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class,response.getClass());
+        assertEquals(UserDTO.class,response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(PASSWORD, response.getBody().getPassword());
     }
 
     @Test
